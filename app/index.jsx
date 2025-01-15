c1e68088aa90edd2ef69842b2155812fd6033cfc
Sandeep <sandeepeos04@gmail.com>
2025-01-15 18:26:32 +0530

    Added eslint and prettier config files and packages, added Searchbar from the package react-native-paper

--- app/index.jsx
+++ app/index.jsx
@@ -1,31 +1,42 @@
-import { View, StyleSheet, Text, SafeAreaView, Platform } from "react-native";
+import { useState } from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import { Searchbar } from 'react-native-paper';
 
 export default function HomeScreen() {
+  const [searchQuery, setSearchQuery] = useState('');
   return (
     <SafeAreaView style={{ flex: 1 }}>
-    <View
-      style={{
-        padding: 2,
-        backgroundColor: "green",
-      }}
-    >
-      <Text style={{ padding: 4 }}>Search</Text>
-    </View>
-    <View
-      style={{
-        flex: 1,
-        paddingLeft: 2,
-        backgroundColor: "skyblue",
-      }}
-    >
-      <Text>List</Text>
-    </View>
+      <View
+        style={{
+          flex: 1,
+          flexGrow: 0,
+          alignItems: 'center',
+          justifyContent: 'center',
+          padding: 2,
+          height: 100,
+        }}
+      >
+        <Searchbar
+          placeholder="Search"
+          onChangeText={setSearchQuery}
+          value={searchQuery}
+        />
+      </View>
+      <View
+        style={{
+          flex: 1,
+          paddingLeft: 2,
+          backgroundColor: 'skyblue',
+        }}
+      >
+        <Text>List</Text>
+      </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  titleContainer: {},
-  stepContainer: {},
-  reactLogo: {},
-});
+// const styles = StyleSheet.create({
+//   titleContainer: {},
+//   stepContainer: {},
+//   reactLogo: {},
+// });
